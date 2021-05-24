3933a4ad718f61dd53d1b1fb1feeb666d2b9578a
luizfelipecs <>
2021-05-24 17:50:35 +0000

    concluido_v1.0

--- haskell01parte1.hs
+++ haskell01parte1.hs
@@ -1,2 +1,34 @@
 -- Prática 01 de Haskell - Parte 1
--- Nome: 
+-- Nome: Luiz Felipe Cavalheiro
+
+sumSquares :: Int -> Int -> Int
+sumSquares x y = x^2 + y^2
+
+circleArea :: Float -> Float
+circleArea r = 2*pi*r^2
+
+age :: Int -> Int -> Int
+age born current = current - born
+
+isElderly :: Int -> Bool
+isElderly age = if age > 65 then True else False
+
+htmlItem :: String -> String
+htmlItem link = "<li>" ++ link ++ "</li>"
+
+startsWithA :: String -> Bool
+startsWithA word = if head word == 'A' then True else False
+
+isVerb :: String -> Bool
+isVerb verb = if last verb == 'r' then True else False
+
+isVowel :: Char -> Bool
+isVowel v = if (v == 'a' || v == 'e' || v == 'i' || v == 'o' || v == 'u') ||
+               (v == 'A' || v == 'E' || v == 'I' || v == 'O' || v == 'U')
+              then True else False
+
+hasEqHeads :: [Int] -> [Int] -> Bool
+hasEqHeads list1 list2 = if head list1 == head list2 then True else False
+
+isVowel2 :: Char -> Bool
+isVowel2 c = if (elem c "aeiou" || elem c "AEIOU") then True else False
